4b9b67becd6089f496acb0f492be57be1ffae420
Yoni Fraimorice <yonifra@gmail.com>
2025-12-21 09:25:45 +0200

    Refactor comments and formatting for consistency in main.py

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ import motor, motor_pair, time
 import runloop, sys, math
 
 # Constants - קבועים גלובליים בשימוש
-WHEEL_CIRCUMFERENCE = 17.5 # לגלגל קטן
-WHEEL_BASE = 11.5 # מרחק בין הגלגלים cm
+WHEEL_CIRCUMFERENCE = 17.5  # לגלגל קטן
+WHEEL_BASE = 11.5  # מרחק בין הגלגלים cm
 # Distance from wheel axle to robot's center of mass in cm (forward)
 CENTER_OFFSET = -4.0
 
@@ -20,7 +20,8 @@ def cm_to_degrees(distance_cm):
     # (distance / circumference) * 360 degrees per rotation
     return (abs(distance_cm) / WHEEL_CIRCUMFERENCE) * 360
 
-#פונקציה לסיבוב הרובוט עם ג׳יירו
+
+# פונקציה לסיבוב הרובוט עם ג׳יירו
 async def turn_pi(turn_degrees: int, speed: int):
     """
     Executes a precise point turn using a Proportional-Integral (PI) controller with yaw feedback.
@@ -30,34 +31,34 @@ async def turn_pi(turn_degrees: int, speed: int):
     :param ki: The integral constant (tune this value, start around 0.01 to 0.1).
     """
 
-    #המתנה 0.05 שניות
+    # המתנה 0.05 שניות
     await runloop.sleep_ms(50)
 
     # איפוס ה-Yaw
     motion_sensor.reset_yaw(0)
 
-    #מקדמי בקרה
+    # מקדמי בקרה
     kp: float = 2.5
     ki: float = 0.05
     # קביעת מהירות הסיבוב ההתחלתית
     speed = 40
-    Vel_R=speed
-    Vel_L=speed
+    Vel_R = speed
+    Vel_L = speed
 
-    #חישוב השגיאה
-    error_PID = abs(turn_degrees - motion_sensor.tilt_angles()[0]/10)
+    # חישוב השגיאה
+    error_PID = abs(turn_degrees - motion_sensor.tilt_angles()[0] / 10)
 
-    #Conrtol loop - לולאת הבקרה
-    while error_PID>1:
+    # Conrtol loop - לולאת הבקרה
+    while error_PID > 1:
         # Give the sensor a moment to reset
         time.sleep_ms(10)
-        #Calculate the correction needed
-        error_PID= abs(turn_degrees - motion_sensor.tilt_angles()[0]/10)
+        # Calculate the correction needed
+        error_PID = abs(turn_degrees - motion_sensor.tilt_angles()[0] / 10)
 
-        #חשוב התיקון הנדרש * המקדם בקרה
-        Correction = int(error_PID*kp)
+        # חשוב התיקון הנדרש * המקדם בקרה
+        Correction = int(error_PID * kp)
 
-        #בדיקת כיוון הסיבוב - חיובי=שמאלה, שלילי=ימינה
+        # בדיקת כיוון הסיבוב - חיובי=שמאלה, שלילי=ימינה
         if turn_degrees > 0:
             Vel_R = Correction
             Vel_L = -Correction
@@ -65,13 +66,24 @@ async def turn_pi(turn_degrees: int, speed: int):
             Vel_R = -Correction
             Vel_L = Correction
 
-        #Yaw measurment: 1 degree = 10 decidegrees
-        #Yaw, Pitch, Roll = motion_sensor.tilt_angles()
-        Yaw = motion_sensor.tilt_angles()[0]/10
-        print('Left Vel:', Vel_L, 'Right Vel:', Vel_R, 'Error:', error_PID, 'Correction:', Correction, 'Yaw:',Yaw)
+        # Yaw measurment: 1 degree = 10 decidegrees
+        # Yaw, Pitch, Roll = motion_sensor.tilt_angles()
+        Yaw = motion_sensor.tilt_angles()[0] / 10
+        print(
+            "Left Vel:",
+            Vel_L,
+            "Right Vel:",
+            Vel_R,
+            "Error:",
+            error_PID,
+            "Correction:",
+            Correction,
+            "Yaw:",
+            Yaw,
+        )
 
         # turn at default velocity
-        motor_pair.move_tank(motor_pair.PAIR_1,Vel_L,Vel_R)
+        motor_pair.move_tank(motor_pair.PAIR_1, Vel_L, Vel_R)
 
     motor_pair.stop(motor_pair.PAIR_1)
 
@@ -138,7 +150,18 @@ async def move_tank_for_cm(move_cm, speed_per=50):
         Vel_R = Vel_R - correction
 
         # Debug output
-        print('Left Vel:', Vel_L, 'Right Vel:', Vel_R, 'Yaw:', current_yaw, 'Distance:', int(avg_pos), '/', int(degrees_to_move))
+        print(
+            "Left Vel:",
+            Vel_L,
+            "Right Vel:",
+            Vel_R,
+            "Yaw:",
+            current_yaw,
+            "Distance:",
+            int(avg_pos),
+            "/",
+            int(degrees_to_move),
+        )
 
         # Drive the motors
         motor_pair.move_tank(motor_pair.PAIR_1, Vel_L, Vel_R)
@@ -147,72 +170,70 @@ async def move_tank_for_cm(move_cm, speed_per=50):
     motor_pair.stop(motor_pair.PAIR_1)
 
 
-#פונקציה להזזת המנוע הימני A
+# פונקציה להזזת המנוע הימני A
 async def turn_right_arm(degrees_turn: int, speed_per: int):
     # Move Side Motor A backward (-270 degrees)
-    fast=int(speed_per * 10)
+    fast = int(speed_per * 10)
     await motor.run_for_degrees(port.A, degrees_turn, fast)
 
 
-#פונקציה להזזת המנוע הימני B
+# פונקציה להזזת המנוע הימני B
 async def turn_left_arm(degrees_turn: int, speed_per: int):
     # Move Side Motor A backward (-270 degrees)
-    fast=int(speed_per * 10)
+    fast = int(speed_per * 10)
     await motor.run_for_degrees(port.B, degrees_turn, fast)
-
-
 
 
 # This is how to write the missions code. After writing the code, run it at the main function.
 async def mission_one_and_two():
     print("--- Starting Mission 1+2 ---")
     # Move forward 10 cm
-    #await move_tank_for_cm(10, 20)
-    #סיבוב ימינה 90
-    print('Turning Left')
+    # await move_tank_for_cm(10, 20)
+    # סיבוב ימינה 90
+    print("Turning Left")
     await turn_pi(-90, 40)
 
-    print('Turning Right')
+    print("Turning Right")
     await turn_pi(90, 40)
 
     # arm function here...
-    await turn_right_arm(90,50)
-    await turn_right_arm(-90,50)
-    await turn_left_arm(-270,50)
+    await turn_right_arm(90, 50)
+    await turn_right_arm(-90, 50)
+    await turn_left_arm(-270, 50)
 
-async def mission_three_and_four(): #missions 3+4
+
+async def mission_three_and_four():  # missions 3+4
     print("--- Starting Mission 3+4 ---")
     # Move forward 10 cm
-    #await move_tank_for_cm(-80, 55)
-    #סיבוב ימינה 90
-    #print('Turning Left')
-    #await turn_pi(-90, 40)
+    # await move_tank_for_cm(-80, 55)
+    # סיבוב ימינה 90
+    # print('Turning Left')
+    # await turn_pi(-90, 40)
 
-    #print('Turning Right')
-
+    # print('Turning Right')
 
     # arm function here...
-    #await turn_right_arm(90,50)
-    #await turn_right_arm(-90,50)
-    #await turn_left_arm(-270,50)
-    #await turn_pi(-90,40)
+    # await turn_right_arm(90,50)
+    # await turn_right_arm(-90,50)
+    # await turn_left_arm(-270,50)
+    # await turn_pi(-90,40)
 
-    #await move_tank_for_cm(-15, 50)
+    # await move_tank_for_cm(-15, 50)
 
-    #await turn_pi(-45, 40)
+    # await turn_pi(-45, 40)
 
-    #await move_tank_for_cm(34, 30)
+    # await move_tank_for_cm(34, 30)
 
-    #await turn_pi(45, 40)
+    # await turn_pi(45, 40)
 
     await move_tank_for_cm(-8, 25)
     await turn_left_arm(-90, 50)
     await move_tank_for_cm(-3, 25)
-    await turn_left_arm(200,50)
+    await turn_left_arm(200, 50)
 
-    #await turn_pi(-2,30)
+    # await turn_pi(-2,30)
 
-    await move_tank_for_cm(11.5,20)
+    await move_tank_for_cm(11.5, 20)
 
 
 async def main():
